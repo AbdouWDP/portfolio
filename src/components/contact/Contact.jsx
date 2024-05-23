48b3f2436f6239d5efac038ff80164e5e462485f
AbdouTheWD <gachtou3@gmail.com>
2024-05-23 02:29:54 +0100

    finish portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,81 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+import { MdOutlineMail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
+import { TbBrandFiverr, TbSend } from "react-icons/tb";
+import ContactForm from "./ContactForm";
+
+const SingleContact = ({ title, contact, icon }) => {
+  return (
+    <div className="single-contact email w-full bg-white rounded-xl flex justify-center items-center">
+      <div className="w-4/5 h-4/5 text-title text-base flex flex-col justify-evenly items-center">
+        <div className="text-3xl">
+          <span>{icon}</span>
+        </div>
+        <div>
+          <p>{title}</p>
+        </div>
+        <div className="text-text">
+          <p>{contact}</p>
+        </div>
+        <div className="write-me-button">
+          <button className="flex gap-1 items-center text-text text-sm">
+            <span>Write me</span>
+            <span className="wmi duration-200 ease-in-out">
+              <FaArrowRightLong />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 function Contact() {
   return (
     <section className="contact w-screen h-screen" id="contact">
-      <article className="w-4/5 h-full m-auto bg-blue-200">
+      <article className="w-4/5 h-full m-auto">
         <div className="h-1/5">
           <div className="text-4xl text-title font-bold text-center">
             <h1>Contact Me</h1>
           </div>
           <div className="text-text text-center my-2">
             <p>Get in touch</p>
+          </div>
+        </div>
+        <div className="w-full h-3/4 m-auto flex justify-center gap-10">
+          <div className="talk-to-me h-full text-center flex justify-between flex-col">
+            <div className="text-title text-2xl font-semibold">
+              <h2>Talk To Me</h2>
+            </div>
+            <SingleContact
+              title="Fiverr"
+              contact="AWDP"
+              icon={<TbBrandFiverr />}
+            />
+            <SingleContact
+              title="Email"
+              contact="gachtou12@gmail.com"
+              icon={<MdOutlineMail />}
+            />
+            <SingleContact
+              title="WhatsApp"
+              contact="0674712007"
+              icon={<FaWhatsapp />}
+            />
+          </div>
+          <div className="write-project-form h-full text-center flex justify-between flex-col">
+            <div className="text-title text-2xl text-center font-semibold">
+              <h2>Write me your project</h2>
+            </div>
+            <ContactForm />
+            <div className="send-message-button w-3/5 h-16 text-lg font-semibold">
+              <button className="w-full h-full text-white flex items-center justify-center gap-2 bg-title hover:bg-title-dark rounded-xl">
+                <span>Send Message</span>
+                <span className="text-xl">
+                  <TbSend />
+                </span>
+              </button>
+            </div>
           </div>
         </div>
       </article>
---
+++ src/components/contact/ContactForm.jsx
@@ -0,0 +1,34 @@
+import { TbSend } from "react-icons/tb";
+
+function ContactForm() {
+  return (
+    <form className="text-text flex flex-col justify-center gap-8 h-full">
+      <div className="inp name w-full relative text-base">
+        <p className="absolute left-4 bg-body px-2">Name</p>
+        <input
+          type="text"
+          placeholder="Insert your name"
+          className="rounded-xl h-16 w-full px-6 bg-body outline-none"
+        />
+      </div>
+      <div className="inp mail w-full relative text-base">
+        <p className="absolute left-4 bg-body px-2">Email</p>
+        <input
+          type="email"
+          placeholder="Insert your email"
+          className="rounded-xl h-16 w-full px-6 bg-body outline-none"
+        />
+      </div>
+      <div className="inp project w-full relative text-base">
+        <p className="absolute left-4 bg-body px-2">Project</p>
+        <textarea
+          type="text"
+          placeholder="Insert your project"
+          className="rounded-xl h-40 w-full px-6 pt-4 bg-body resize-none outline-none"
+        ></textarea>
+      </div>
+    </form>
+  );
+}
+
+export default ContactForm;
