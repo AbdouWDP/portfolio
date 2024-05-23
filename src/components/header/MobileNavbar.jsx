7a99d81996fa4984800ee037f6d32e57adf0cced
AbdouTheWD <gachtou3@gmail.com>
2024-05-23 05:05:28 +0100

    up to date

--- src/components/header/MobileNavbar.jsx
+++ src/components/header/MobileNavbar.jsx
@@ -1,16 +1,18 @@
-import { GrIndicator } from "react-icons/gr";
+import { LuLayoutGrid } from "react-icons/lu";
 
 function MobileNavbar() {
   return (
-    <header className="mobile-navbar h-10 sticky top-0">
+    <header className="mobile-navbar h-16 sticky top-0 bg-white shadow-md z-50">
       <div className="w-11/12 h-full m-auto flex justify-between items-center text-title text-xl font-bold">
         <div>
           <p>AWDP</p>
         </div>
         <div>
-          <span>
-            <GrIndicator />
-          </span>
+          <button className="text-2xl">
+            <span>
+              <LuLayoutGrid />
+            </span>
+          </button>
         </div>
       </div>
     </header>
